2fae9d13c511b9282455340c64e942c3e2bb7758
hunomina <hugo.f972@gmail.com>
2021-06-23 18:20:31 +0200

    Add setting form

--- web/index.js
+++ web/index.js
@@ -2,19 +2,70 @@ import * as wasm from "epidemic-simulator";
 import p5 from 'p5';
 import { getSketch } from "./sketch";
 
-let simulation = wasm.init_simulation(
-    new wasm.Configuration(
-        new wasm.Size(40, 40),
-        new wasm.SubjectsRepartition(20, 5),
-        new wasm.Rules(1, 2)
-    ),
-    100
-);
+function getSimulationModel() {
+    return wasm.init_simulation(
+        new wasm.Configuration(
+            new wasm.Size(
+                get_input_value('simulator_height_input'),
+                get_input_value('simulator_width_input'),
+            ),
+            new wasm.SubjectsRepartition(
+                get_input_value('sick_percentage_input'),
+                get_input_value('recovered_percentage_input'),
+            ),
+            new wasm.Rules(
+                get_input_value('movement_speed_input'),
+                get_input_value('safe_distance_input', 'float'),
+            )
+        ),
+        get_input_value('subject_count_input')
+    );
+}
 
-new p5(getSketch(simulation, {
-    fps: 20,
-    size: {
-        width: 800,
-        height: 800
+function init_simulation(simulationModel) {
+    return new p5(
+        getSketch(
+            simulationModel,
+            {
+                fps: 20,
+                size: {
+                    width: 800,
+                    height: 800
+                }
+            }
+        ),
+        'simulationCanvas'
+    );
+}
+
+/**
+ * @param {Event} event 
+ */
+function resetSimulation(event) {
+    event.preventDefault();
+    simulation.remove();
+    simulation = init_simulation(getSimulationModel());
+}
+
+function get_input_value(id, type = 'int') {
+    const value = document.getElementById(id).value;
+    if (value === '') {
+        return null;
     }
-}));
+    switch (type) {
+        case 'int':
+            return parseInt(value);
+        case 'float':
+            return parseFloat(value);
+        default:
+            return value;
+    }
+}
+
+var simulation;
+(() => {
+    document.getElementById('settingsForm').addEventListener('submit', resetSimulation);
+    simulation = init_simulation(getSimulationModel());
+})();
+
+
